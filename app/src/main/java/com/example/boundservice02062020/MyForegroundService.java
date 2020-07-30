3ab260276cecc8f65a818141c1f5549cbe9696b8
phatdroid94 <phatdroid94@gmail.com>
2020-07-30 18:45:37 +0700

    foreground service

--- app/src/main/java/com/example/boundservice02062020/MyForegroundService.java
+++ app/src/main/java/com/example/boundservice02062020/MyForegroundService.java
@@ -7,6 +7,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -15,6 +16,8 @@ import androidx.core.app.NotificationCompat;
 public class MyForegroundService extends Service {
 
     String CHANNEL_ID = "mychannel_1";
+    NotificationCompat.Builder mBuilder;
+    int count = 0;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -24,14 +27,29 @@ public class MyForegroundService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mBuilder = createNotification();
+        startForeground(1 , mBuilder.build());
+    }
 
+    @Override
+    public int onStartCommand(Intent intent, final int flags, int startId) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                count++;
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(1,createNotification().build());
+                new Handler().postDelayed(this, 1000);
+            }
+        },1000);
+        return super.onStartCommand(intent, flags, startId);
     }
 
     private NotificationCompat.Builder createNotification(){
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this, CHANNEL_ID);
-        builder.setContentTitle("Xu ly tuong tac notification");
-        builder.setContentText("Hay chon cac option ben duoi");
+        builder.setContentTitle("Service running");
+        builder.setContentText("Count " + count);
         builder.setStyle(new NotificationCompat.BigTextStyle());
         builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setOngoing(true);
